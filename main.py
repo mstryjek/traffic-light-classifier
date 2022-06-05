a3618981cdf4d631b2a23932fe64a5f35a72fe97
mstryjek <73649466+mstryjek@users.noreply.github.com>
2022-06-05 11:28:38 +0200

    feat: Upload final version (Cohen's Kappa)

--- main.py
+++ main.py
@@ -9,18 +9,20 @@ import glob
 from preprocessing import pipeline
 from visualization import draw_light_class
 
+from tensorflow_addons.metrics import CohenKappa
+
 
 def main() -> None:
 	IMG_PATH = 'test_resources'
 	MODEL_PATH = 'models'
-	
+
 	model = keras.models.load_model(os.path.join(MODEL_PATH, 'TLC.h5'))
 	image_files = glob.glob(os.path.join(IMG_PATH, '*.jpg'))
 	images = []
 
-	for img in image_files:	
+	for img in image_files:
 		images.append(cv2.imread(img, cv2.IMREAD_COLOR))
-	
+
 	images_preprocessed = pipeline(images)
 
 	predictions = model.predict(images_preprocessed)
@@ -30,7 +32,7 @@ def main() -> None:
 	for img, cls_ in zip(images, classes):
 		img_drawn = draw_light_class(img, cls_)
 		drawn_images.append(img_drawn)
-	
+
 	final_img = np.concatenate(drawn_images, axis=1)
 
 	plt.imshow(cv2.cvtColor(final_img, cv2.COLOR_BGR2RGB))
